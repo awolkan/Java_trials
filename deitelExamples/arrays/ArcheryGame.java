236c28ff4a82906bfb6c8f1824f7b45259dd40f5
awolkan <avolkan@gmail.com>
2025-10-07 18:05:25 +0000

    Added array exercise

---
+++ deitelExamples/arrays/ArcheryGame.java
@@ -0,0 +1,64 @@
+
+import java.util.Scanner;
+import java.util.Random;
+
+class ArcheryGame {
+
+    public static void simulateGame(int plyCount, int[][] scores) {
+
+        Random random = new Random();
+        for (int i=0; i<plyCount; i++) {
+            for (int j=0; j<3; j++) {
+                scores[i][j] = random.nextInt(10) + 1;
+            }
+        }
+    }
+
+    public static void printScores(int[][] scores) {
+        int players = scores.length;
+        int winner = 0;
+        int winsum = 0;
+        int sum = 0;
+        System.out.println("Scores: S1 S2 S3 Sum");
+        for (int i=0; i<players; i++) {
+            System.out.printf("%s%d %3d%3d%3d%4d%n", "Player", i+1, scores[i][0], 
+                scores[i][1], scores[i][2], sum = scores[i][0] + scores[i][1] 
+                + scores[i][2] );
+            if (sum > winsum) {
+                winsum = sum;
+                winner = i+1;
+            }
+        }
+        System.out.println("");
+        System.out.println("Winner is: " + winner);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int players = 0;
+
+        System.out.print("Enter the number of players: ");
+        while (true) {
+            try {
+                String input = scanner.nextLine();
+                players = Integer.parseInt(input);
+            } catch (Exception e) {
+                System.err.println("Error: " + e.getMessage());
+                System.out.println("Please enter a number:");
+            }
+            break;
+        }
+        scanner.close();
+        if (players == 0) {
+            System.out.println("No players selected, app quits");
+            return;
+        }
+
+        int[][] scoreArray = new int[players][3]; 
+        simulateGame(players, scoreArray);
+        printScores(scoreArray);
+
+    }
+
+}
